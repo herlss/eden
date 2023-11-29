58ea645612b89f212e099a21e9ac445a33cbfa4a
Matheus Henrique Cavalcante Durães Dias <matheusdias-ieg@instituto.ad>
2023-11-29 15:28:13 -0300

    router

--- app/api/routers/status.py
+++ app/api/routers/status.py
@@ -1,3 +1,20 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+@router.get('/')
+def test():
+    return 'whatever'
+
+
+@router.get('/aws/{loc}')
+def getAwsStatus(loc):
+    return f'function that will return AWS status page on {loc}'
+
+@router.get('/jira/{loc}')
+def getJiraStatus(loc):
+    return f'function that will return Jira status page on {loc}'
+
+@router.get('/oci/{loc}')
+def getOciStatus(loc):
+    return f'function that will return OCI status page on {loc}'
