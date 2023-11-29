6def0ae5b5dbb675fff22ad52ea42bba7e59dc3e
Matheus Henrique Cavalcante Durães Dias <matheusdias-ieg@instituto.ad>
2023-11-29 16:36:53 -0300

    route fix

--- app/api/routers/status.py
+++ app/api/routers/status.py
@@ -7,14 +7,14 @@ def test():
     return 'whatever'
 
 
-@router.get('/aws/{loc}')
-def getAwsStatus(loc):
-    return f'function that will return AWS status page on {loc}'
+@router.get('/aws')
+def getAwsStatus():
+    return f'function that will return AWS status page'
 
-@router.get('/jira/{loc}')
-def getJiraStatus(loc):
-    return f'function that will return Jira status page on {loc}'
+@router.get('/jira')
+def getJiraStatus():
+    return f'function that will return Jira status page'
 
-@router.get('/oci/{loc}')
-def getOciStatus(loc):
-    return f'function that will return OCI status page on {loc}'
+@router.get('/oci')
+def getOciStatus():
+    return f'function that will return OCI status page'
