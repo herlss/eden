5358d8baa335cf865a2c0d71072d9cdd759e1fbf
Henrique Miranda Paulino <henriquepaulino-ieg@instituto.ad>
2023-11-30 13:49:39 -0300

    Added cors for api

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from app.api.routers import status, dashboard
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(status.router)
 app.include_router(dashboard.router)
+
+
+origins = [
+    "https://eden-front.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
